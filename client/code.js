2a084cd2e2b1c8dffd3bf255a888d90a41617715
Paul Rodwell <paul.rodwell@btinternet.com>
2025-04-30 11:40:59 +0100

    don't pin to specific version, just the major version

--- client/code.js
+++ client/code.js
@@ -6,18 +6,16 @@
  */
 
 async function emit($item, item) {
-  if (!$("link[href='https://cdn.jsdelivr.net/npm/highlight.js@11.7.0/styles/github.min.css']").length) {
-    $('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/highlight.js@11.7.0/styles/github.min.css">').appendTo("head")
+  if (!$("link[href='https://cdn.jsdelivr.net/npm/highlight.js@11/styles/github.min.css']").length) {
+    $('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/highlight.js@11/styles/github.min.css">').appendTo("head")
   }
   if (!$("link[href='/plugins/code/code.css']").length) {
     $('<link rel="stylesheet" href="/plugins/code/code.css">').appendTo("head")
   }
 
-  window.HighlightJS = (await import('https://cdn.jsdelivr.net/npm/highlight.js@11.7.0/+esm')).HighlightJS
+  window.HighlightJS = (await import('https://cdn.jsdelivr.net/npm/highlight.js@11/+esm')).HighlightJS
 
   $item.html(`<pre class='hljs'><code class='hljs'>${HighlightJS.highlightAuto(item.text).value}</code></pre>`)
-
-
 }
 
 function bind($item, item) {
